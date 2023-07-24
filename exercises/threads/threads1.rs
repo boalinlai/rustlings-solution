ac360b79738bc6fcb6e377927dfc95c12567ad6d
Boa-Lin <boalinlai@gmail.com>
2023-07-23 23:33:50 -0700

    exercise/*t

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -8,7 +8,6 @@
 // Execute `rustlings hint threads1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::thread;
 use std::time::{Duration, Instant};
@@ -27,12 +26,13 @@ fn main() {
     let mut results: Vec<u128> = vec![];
     for handle in handles {
         // TODO: a struct is returned from thread::spawn, can you use it?
+        results.push(handle.join().unwrap());
     }
 
     if results.len() != 10 {
         panic!("Oh no! All the spawned threads did not finish!");
     }
-
+    
     println!();
     for (i, result) in results.into_iter().enumerate() {
         println!("thread {} took {}ms", i, result);
